8575f4fba55dd923adbb6ab2a73672d7b0cd1104
Jonas Friedann <j@frd.mn>
2014-08-03 13:36:24 +0200

    Combine variable declaration

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+    ,path = require('path')
+    ,favicon = require('static-favicon')
+    ,logger = require('morgan')
+    ,cookieParser = require('cookie-parser')
+    ,bodyParser = require('body-parser')
+    ,handlebars  = require('express3-handlebars');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index')
+    ,users = require('./routes/users');
 
 var app = express();
 
