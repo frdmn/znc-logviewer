44098247df49a52643e7e6c99ff5651d012f549d
Jonas Friedann <j@frd.mn>
2014-08-03 14:52:12 +0200

    Implement about view

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+    res.render('about', { title: 'About' });
 });
 
 module.exports = router;
