db1e2a149235ab7ccd2e1ead4815b31429f6d0a4
Jonas Friedann <j@frd.mn>
2014-08-09 18:24:20 +0200

    Remove initial log file parsing

--- routes/index.js
+++ routes/index.js
@@ -5,37 +5,6 @@ var router = express.Router();
 // Load settings file
 var settings = require('../settings.json');
     
-/* Parse informations from log file names */
-
-// Init element array
-var channelArray = [],
-    dateArray = [];
-
-var files = fs.readdirSync(settings.zncpath + '/users/' + settings.user + '/moddata/log/');
-
-files.forEach(function(filename){
-    // Split filenames based on a pattern like <network>_<channel>_<date>
-    var splitPattern = '^' + settings.network + '_(.*)_([0-9]*).log$',
-        splitRegex = new RegExp(splitPattern, 'g'),
-        splitMatch = splitRegex.exec(filename);
-
-    // If we have a match, proceed to add to arrays
-    if (splitMatch) {
-        // Remove non-channels (queries) from channel list
-        var channelMatches = splitMatch[1].match(/^#.*$/);
-        if (channelMatches) {
-            channelArray.push(channelMatches[0].substring(1));
-        }
-
-        // Turn into correct format for datepicker
-        var dateRegex = /^([0-9]{4})([0-9]{2})([0-9]{2})$/g,
-            dateMatch = dateRegex.exec(splitMatch[2]);
-        if (dateMatch) {
-            var newDate = dateMatch[1] + '-' + dateMatch[2] + '-' + dateMatch[3];
-            dateArray.push(newDate);
-        }
-    } 
-});
 
 // Make arrays unique
 channelArray = uniquify(channelArray);
