002232f77842647cc6c8999064846c75c428da23
Jonas Friedann <j@frd.mn>
2014-08-10 00:30:18 +0200

    Fix about page

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /* Routes */
 
-app.use('/', routeIndex);
 app.use('/about', routeAbout);
+app.use('/', routeIndex);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
