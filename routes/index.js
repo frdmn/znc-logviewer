91e324b45b3735ff306ce6a04f8026c808fbe39f
Jonas Friedann <j@frd.mn>
2014-08-09 18:25:15 +0200

    Read channels on each GET

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,43 @@ var router = express.Router();
 // Load settings file
 var settings = require('../settings.json');
     
-
-// Make arrays unique
-channelArray = uniquify(channelArray);
-dateArray = uniquify(dateArray);
-
-// Create object which gets passed to the template
-arrayObject = {};
-arrayObject.network = settings.network;
-arrayObject.user = settings.user;
-arrayObject.channelArray = channelArray;
-arrayObject.dateArray = dateArray;
-
-// Display found elements
-console.log('Found ' + channelArray.length + ' channels and ' + dateArray.length + ' possible dates');
-
 /* GET home page. */
 router.get('/', function(req, res) {
+    /* Parse informations from log file names */
+
+    // Init element array
+    var channelArray = [];
+    
+    var files = fs.readdirSync(settings.zncpath + '/users/' + settings.user + '/moddata/log/');
+
+    files.forEach(function(filename){
+        // Split filenames based on a pattern like <network>_<channel>_<date>
+        var splitPattern = '^' + settings.network + '_(.*)_[0-9]*.log$',
+            splitRegex = new RegExp(splitPattern, 'g'),
+            splitMatch = splitRegex.exec(filename);
+
+        // If we have a match, proceed to add to arrays
+        if (splitMatch) {
+            // Remove non-channels (queries) from channel list
+            var channelMatches = splitMatch[1].match(/^#.*$/);
+            if (channelMatches) {
+                channelArray.push(channelMatches[0].substring(1));
+            }
+        } 
+    });
+
+    // Make arrays unique
+    channelArray = uniquify(channelArray);
+
+    // Create object which gets passed to the template
+    arrayObject = {};
+    arrayObject.network = settings.network;
+    arrayObject.user = settings.user;
+    arrayObject.channelArray = channelArray;
+
+    // Display found elements
+    console.log('Found ' + channelArray.length + ' channels');
+
     res.render('index', { 
         title: 'Index',
         active_index: true,
